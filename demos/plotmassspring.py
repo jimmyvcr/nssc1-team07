acd1d57af8e422597a9bda9dcff795cac5bc81d5
Martin Huber <martin.huber@tuwien.ac.at>
2025-11-23 14:26:48 +0100

    configured plotscript and test ode for ImporvedEuler function

--- demos/plotmassspring.py
+++ demos/plotmassspring.py
@@ -1,24 +1,26 @@
 import numpy as np
-data = np.loadtxt('../build/output_test_ode.txt', usecols=(0, 1, 2))
+
+data = np.loadtxt("../build/output_test_ode.txt", usecols=(0, 1, 2))
 # print (data)
 
 import matplotlib.pyplot as plt
 
-plt.plot(data[:,0], data[:,1], label='position')
-plt.plot(data[:,0], data[:,2], label='velocity')
-plt.xlabel('time')
-plt.ylabel('value')
-plt.title('Mass-Spring System Time Evolution')
+plt.plot(data[:, 0], data[:, 1], label="position")
+plt.plot(data[:, 0], data[:, 2], label="velocity")
+plt.xlabel("time")
+plt.ylabel("value")
+plt.title("Mass-Spring System Time Evolution, Improved Euler (10*tend, 10*steps)")
 plt.legend()
 plt.grid()
+plt.savefig("mass_spring_time_evolution_10tend_10steps.png")
 plt.show()
 
 
-plt.plot(data[:,1], data[:,2], label='phase plot')
-plt.xlabel('position')
-plt.ylabel('velocity')
-plt.title('Mass-Spring System Phase Plot')
+plt.plot(data[:, 1], data[:, 2], label="phase plot")
+plt.xlabel("position")
+plt.ylabel("velocity")
+plt.title("Mass-Spring System Phase Plot, Improved Euler (10*tend, 10*steps)")
 plt.legend()
 plt.grid()
+plt.savefig("mass_spring_phase_10tend_10steps.png")
 plt.show()
-
--- demos/test_ode.cpp
+++ demos/test_ode.cpp
@@ -36,14 +36,15 @@ public:
 
 int main()
 {
-  double tend = 4*M_PI;
-  int steps = 100;
+  double tend = 4*M_PI*10;
+  int steps = 100*10;
   double tau = tend/steps;
 
   Vector<> y = { 1, 0 };  // initializer list
   auto rhs = std::make_shared<MassSpring>(1.0, 1.0);
   
-  ExplicitEuler stepper(rhs);
+  // ExplicitEuler stepper(rhs);
+  ImprovedEuler stepper(rhs);
   // ImplicitEuler stepper(rhs);
 
   std::ofstream outfile ("output_test_ode.txt");
