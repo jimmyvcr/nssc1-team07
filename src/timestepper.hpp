d86a8d33993fb67e46acf56f086ac729bb128776
Anton Zamyatin <anton.zamyatin@tuwien.ac.at>
2025-11-24 12:45:50 +0100

    implement Crank-Nicolson stepper

--- src/timestepper.hpp
+++ src/timestepper.hpp
@@ -61,6 +61,28 @@ class ImplicitEuler : public TimeStepper {
     NewtonSolver(m_equ, y);
   }
 };
+
+class CrankNicolson : public TimeStepper
+{
+  std::shared_ptr<NonlinearFunction> m_equ;
+  std::shared_ptr<Parameter> m_tau;
+  std::shared_ptr<ConstantFunction> m_yold;
+public:
+  CrankNicolson(std::shared_ptr<NonlinearFunction> rhs) 
+    : TimeStepper(rhs), m_tau(std::make_shared<Parameter>(0.0)) 
+  {
+    m_yold = std::make_shared<ConstantFunction>(rhs->dimX());
+    auto ynew = std::make_shared<IdentityFunction>(rhs->dimX());
+    m_equ = ynew - m_yold - m_tau * (0.5 * (Compose(m_rhs, m_yold) + m_rhs));
+  }
+
+  void DoStep(double tau, VectorView<double> y) override
+  {
+    m_yold->set(y);
+    m_tau->set(tau);
+    NewtonSolver(m_equ, y);
+  }
+};
 }  // namespace ASC_ode
 
 
