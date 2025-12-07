76be92eb45514c64965fe03979d214149300af00
Martin Huber <martin.huber@tuwien.ac.at>
2025-12-07 13:31:06 +0100

    linted mass_spring.cpp

--- mechsystem/mass_spring.cpp
+++ mechsystem/mass_spring.cpp
@@ -1,16 +1,15 @@
 #include "mass_spring.hpp"
 #include "Newmark.hpp"
 
-int main()
-{
+int main() {
   MassSpringSystem<2> mss;
-  mss.setGravity( {0,-9.81} );
-  auto fA = mss.addFix( { { 0.0, 0.0 } } );
-  auto mA = mss.addMass( { 1, { 1.0, 0.0 } } );
-  mss.addSpring ( { 1, 10, { fA, mA } }  );
+  mss.setGravity({0, -9.81});
+  auto fA = mss.addFix({{0.0, 0.0 }};
+  auto mA = mss.addMass({1, {1.0, 0.0}});
+  mss.addSpring({1, 10, {fA, mA}});
 
-  auto mB = mss.addMass( { 1, { 2.0, 0.0 } } );
-  mss.addSpring ( { 1, 20, { mA, mB } } );
+  auto mB = mss.addMass({1, {2.0, 0.0}});
+  mss.addSpring({1, 20, {mA, mB}});
 
   std::cout << "mss: " << std::endl << mss << std::endl;
 
@@ -25,9 +24,9 @@ int main()
   auto mss_func = std::make_shared<MSS_Function<2>> (mss);
   auto mass = std::make_shared<IdentityFunction> (x.size());
 
-  mss.getState (x, dx, ddx);
-  
+  mss.getState(x, dx, ddx);
+
   SolveODE_Newmark(tend, steps, x, dx,  mss_func, mass,
-                   [](double t, VectorView<double> x) { std::cout << "t = " << t
-                                                             << ", x = " << Vec<4>(x) << std::endl; });
+    [](double t, VectorView<double> x) { std::cout << "t = "
+     << t << ", x = " << Vec<4>(x) << std::endl; });
 }
